6c772bdae65a07875456abb78d6efa99758421bc
nyagato_00 <nyagato.eva@gmail.com>
2022-08-12 13:34:17 +0900

    feat: `app`という名前のfreshのプロジェクトを作成
    info
    ```
    ❯ deno run -A --no-check https://raw.githubusercontent.com/lucacasonato/fresh/main/init.ts app
    Download https://raw.githubusercontent.com/lucacasonato/fresh/main/init.ts
    Download https://raw.githubusercontent.com/lucacasonato/fresh/main/src/dev/deps.ts
    Download https://raw.githubusercontent.com/lucacasonato/fresh/main/src/dev/error.ts
    Download https://raw.githubusercontent.com/lucacasonato/fresh/main/src/dev/mod.ts
    Download https://deno.land/std@0.150.0/flags/mod.ts
    Download https://deno.land/std@0.150.0/fs/walk.ts
    Download https://deno.land/std@0.150.0/path/mod.ts
    Download https://deno.land/std@0.150.0/semver/mod.ts
    Download https://deno.land/std@0.150.0/_util/os.ts
    Download https://deno.land/std@0.150.0/path/_interface.ts
    Download https://deno.land/std@0.150.0/path/common.ts
    Download https://deno.land/std@0.150.0/path/glob.ts
    Download https://deno.land/std@0.150.0/path/posix.ts
    Download https://deno.land/std@0.150.0/path/separator.ts
    Download https://deno.land/std@0.150.0/path/win32.ts
    Download https://deno.land/std@0.150.0/_util/assert.ts
    Download https://deno.land/std@0.150.0/fs/_util.ts
    Download https://deno.land/std@0.150.0/path/_constants.ts
    Download https://deno.land/std@0.150.0/path/_util.ts
    Do you want to use 'twind' (https://twind.dev/) for styling? [y/N] y
    Do you use VS Code? [y/N] y
    The manifest has been generated for 3 routes and 1 islands.
    
    Project created!
    
    In order to start the development server, run:
    
    $ cd app
    $ deno task start
    ```

---
+++ app/main.ts
@@ -0,0 +1,12 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.js";
+
+await start(manifest, { plugins: [twindPlugin(twindConfig)] });
